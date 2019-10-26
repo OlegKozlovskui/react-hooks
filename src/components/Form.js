bc23178a1b91eb80eecf4ab190a4d9a2177807c6
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-10-26 11:26:58 +0300

    useState, custom hook useForm

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useFrom } from '../hooks/useForm';
+
+const Form = () => {
+  const [values, handleChange] = useFrom({ email: '', password: '' });
+  console.log(handleChange);
+
+  return (
+    <form>
+      <input
+        type="email"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+export default Form;
