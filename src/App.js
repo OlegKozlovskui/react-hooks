d017acf8f13f60197da78699875da9793a94f8cd
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-10-26 14:49:35 +0300

    useLayoutEffect, custom hook useMeasure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form';
-import Posts from './components/Posts';
 
 function App() {
   return (
     <div className="App">
       <Form />
-      <Posts />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { useFrom } from '../hooks/useForm';
+import { useMeasure } from '../hooks/useMeasure';
 
 const Form = () => {
-  const [values, handleChange] = useFrom({ email: '', password: '' });
+  const [values, handleChange] = useFrom({ email: '', password: '', bio: '' });
+  const [rect, bioRef] = useMeasure([values.bio]);
   
   return (
-    <form>
-      <input
-        type="email"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </form>
+    <>
+      <strong ref={bioRef}>
+        {values.bio}
+      </strong>
+      <pre>
+        {JSON.stringify(rect, null, 2)}
+      </pre>
+      <hr/>
+      <form>
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <textarea
+          name="bio"
+          value={values.bio}
+          onChange={handleChange}
+        />
+      </form>
+    </>
   );
 };
 
