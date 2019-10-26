87db753c72e9281fce94e390723d05ef4479cbe2
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-10-26 14:19:31 +0300

    useRef, improved useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,13 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export const useFetch = (url) => {
+  const isCurrent = useRef(true);
   const [state, setState] = useState({ data: null, loading: true });
+
+  useEffect(() => {
+    return () => {
+      isCurrent.current = false;
+    }
+  }, []);
 
   useEffect(() => {
     setState(state => ({ data: state.data, loading: true }));
     fetch(url)
       .then(x => x.json())
-      .then(y => setState({ data: y, loading: false }));
+      .then(y => {
+        if (isCurrent) setState({ data: y, loading: false });
+      });
   }, [url]);
 
   return state;
